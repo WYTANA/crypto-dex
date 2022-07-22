b73c24c0362d01f015c584497b0f37c866fbcc1f
Travis L Orback <tlorback@gmail.com>
2022-07-21 21:57:41 -0600

    Add .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-};
+}
