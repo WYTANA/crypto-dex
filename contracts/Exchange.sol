f4f0eb07fe2e39f2629f4cbaeb27064e5bcd90e8
torback <tlorback@gmail.com>
2022-08-16 16:09:31 -0600

    Create deposit functionality and tests for tokens in exchange contract

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,13 +2,40 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";
 
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
+    // For each token address we see how many tokens belong to each user address
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+    // Deposit and Withdraw Token
+
+    function depositToken(address _token, uint256 _amount) public {
+        // Transfer tokens to the exchange
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+
+        // Emit an Event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    // Check balances with wrapper function
+    function balanceOf(address _token, address _user)
+        public
+        view
+        returns (uint256)
+    {
+        return tokens[_token][_user];
     }
 }
